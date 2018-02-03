225fc32b71babd934b88d269732a711fae6f7d2a
Duckslife <ejrgus3@gmail.com>
2018-02-03 22:06:55 +0900

    dummy posts

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from app import app
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return"Hello. World!"
+	user = {'username':'Duckslife'}
+	posts = [
+		{
+			'author':{'username':'john'},
+			'body':'test test test test'
+		},
+		{	
+			'author':{'username':'sarang'},
+			'body': 'test1 test1 test1'
+		}
+	]
+	return render_template('index.html',title='home', user=user, posts=posts)
