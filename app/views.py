09c5b6dd5dde10831fa7469a4a1dcdc968d2a23f
Duckslife <ejrgus3@gmail.com>
2018-02-05 17:40:14 +0900

    config file "flask-wtf"

--- app/views.py
+++ app/views.py
@@ -1,6 +1,15 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect 
+from app.forms import login_form
 
+@app.route('/login', methods = ['GET','POST'])
+def login():
+	form = login_form()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me={}'.format(
+			form.username.data, form.remember_me.data))
+		return redirect('/index')
+	return render_template('login.html', title = 'Sign In', form=form)
 
 @app.route('/')
 @app.route('/index')
@@ -11,7 +20,7 @@ def index():
 			'author':{'username':'john'},
 			'body':'test test test test'
 		},
-		{	
+		{
 			'author':{'username':'sarang'},
 			'body': 'test1 test1 test1'
 		}
