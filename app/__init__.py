c1f2206865211defa9cf324e4ff2e541f1ea3783
Duckslife <ejrgus3@gmail.com>
2018-03-12 13:07:42 +0900

    dbconn

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import MySQLdb
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import views, routes, models
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, flash, redirect 
+from flask import render_template, flash, redirect
 from app.forms import login_form
 
 @app.route('/login', methods = ['GET','POST'])
@@ -8,7 +8,7 @@ def login():
 	if form.validate_on_submit():
 		flash('Login requested for user {}, remember_me={}'.format(
 			form.username.data, form.remember_me.data))
-		return redirect('/index')
+		return redirect(url_for('index'))
 	return render_template('login.html', title = 'Sign In', form=form)
 
 @app.route('/')
